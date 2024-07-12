f44aebe8a789b30e7a2b4e9fbe769ade206c9c2d
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-07-12 17:08:20 +0700

    update security config

--- account-service/src/main/java/com/thinhtran/accountservice/config/SecurityConfig.java
+++ account-service/src/main/java/com/thinhtran/accountservice/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.thinhtran.accountservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -17,13 +23,31 @@ public class SecurityConfig {
             , "/auth/token"
             , "/auth/introspect"};
 
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(request ->
                 request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                         .anyRequest()
                         .authenticated());
+
+        http.oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+
 }
